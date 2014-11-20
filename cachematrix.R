37b058e63de1d2e96ea3c3d886cd7f851dba813a
jmf3d3d <jmf@logic.bm>
2014-11-20 16:21:56 -0400

    submission for assignement 2
    completed code for functions required for assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,76 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will create cached matrix and a function that will create or access the cached matrix
+## 
 
-## Write a short comment describing this function
+## This function takes as input a matrix creates a cached matrix object 
+## assign the output of this function to a new variable to hold the special object
+##  e.g.   foo <- makeCacheMatrix(Matrix_x)
+##  the methods defined below allow access to the matrices in the object using the form
+##   foo$setinv() for example to set the inverted matrix
+## 
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## assigns a new special object to x and sets the inverted matrix to null
 
+  
+      inv <- NULL   # this is the inverted matrix, initialized to NULL
+      
+      set <- function(y) {
+        x <<- y
+        inv <<- NULL
+      }
+      
+  ## these are the methods for setting and getting the components of the cached matrix
+  
+      get <- function()    ##simply returns the original vector
+      
+      setinv <- function(solve) inv <<- solve    ##sets the inv matrix to the inverse of the original vector
+      getinv <- function() inv  ##simply returns the inverted matirx - NULL if not already cached
+  
+  
+      ## this allow access to the function methods defined above
+    
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
+      
+      
 }
 
 
-## Write a short comment describing this function
+## This function accesses the cached object created by makeCachedMatrix
+## to access the cached inverted matrix if it exists or to invert the matrix
+## it returns the inverted matrix
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## first get the existing matrix
+        inverted <- x$getinv()
+        
+        
+  ## check to see if this has been inverted and cached already and simply return it if so
+        if(!is.null(inverted)) {
+              message("getting cached data")  #so we know it was cached
+              return(inverted)
+  }
+      ##otherwise invert the matrix and used methods set it in x
+        data <- x$get()           ## first retrieve the original matrix
+        inverted <- solve(data, ...)  ## invert it
+        x$setinv(inverted)            ## then cache this inverted matrix 
+        inverted    ##return the inverted matrix
+        
+  
+ 
+  
+     
 }
+
+
+
+
+
+
+
+## for testing purposes
+##matrixx <- rbind(c(2, -1/4), c(-1/4, 2)) 
